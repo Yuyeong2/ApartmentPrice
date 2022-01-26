e5b865d3b94e24a0ec4b85d249281e8e1e2503c1
U02 <403467@naver.com>
2022-01-26 09:34:38 +0900

    220125

---
+++ src/main/java/com/koreait/ap/model/LocationCodeEntity.java
@@ -0,0 +1,12 @@
+package com.koreait.ap.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LocationCodeEntity {
+    private int incd;
+    private String areanm;
+    private String excd;
+}
